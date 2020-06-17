a28ec41588678592a5b7380db9cea151fdf47b2f
Amanda Winkles <amandawinkles@gmail.com>
2020-06-16 20:52:22 -0500

    working on server spec

--- server/models/index.js
+++ server/models/index.js
@@ -2,7 +2,10 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
+    // a function which produces all the messages
+    get: function () {
+
+    },
     post: function () {} // a function which can be used to insert a message into the database
   },
 
--- server/spec/server-spec.js
+++ server/spec/server-spec.js
@@ -16,18 +16,23 @@ describe('Persistent Node Chat Server', function() {
     });
     dbConnection.connect();
 
-       var tablename = ""; // TODO: fill this out
+
+       var messagesTable = "messages"; // TODO: fill this out
+       var userTable = "users";
+       var roomsTable = "rooms";
 
     /* Empty the db table before each test so that multiple tests
      * (or repeated runs of the tests) won't screw each other up: */
-    dbConnection.query('truncate ' + tablename, done);
+    // dbConnection.query('truncate ' + messagesTable, done);
+    // dbConnection.query('truncate ' + userTable, done);
+    // dbConnection.query('truncate ' + roomsTable, done);
   });
 
   afterEach(function() {
     dbConnection.end();
   });
 
-  it('Should insert posted messages to the DB', function(done) {
+  xit('Should insert posted messages to the DB', function(done) {
     // Post the user to the chat server.
     request({
       method: 'POST',
@@ -57,7 +62,7 @@ describe('Persistent Node Chat Server', function() {
           expect(results.length).to.equal(1);
 
           // TODO: If you don't have a column named text, change this test.
-          expect(results[0].text).to.equal('In mercy\'s name, three days is all I need.');
+          expect(results[0].message).to.equal('In mercy\'s name, three days is all I need.');
 
           done();
         });
@@ -67,21 +72,43 @@ describe('Persistent Node Chat Server', function() {
 
   it('Should output all messages from the DB', function(done) {
     // Let's insert a message into the db
-       var queryString = "";
+    //Create Query Strings for 3 insert statements (user, room, message table)
+       var queryStringMessages = "INSERT INTO messages SET ?";
+       var queryStringRooms = "INSERT INTO rooms SET ?";
+       var queryStringUsers = "INSERT INTO users SET ?";
+
        var queryArgs = [];
+       //{message: 'teaadfafas', user: user, room: room}
+
+
     // TODO - The exact query string and query args to use
     // here depend on the schema you design, so I'll leave
     // them up to you. */
 
-    dbConnection.query(queryString, queryArgs, function(err) {
+    dbConnection.query(queryStringRooms, {roomname: 'main'}, function (error, results, fields) {
+      if (error) throw error;
+      console.log(results.insertId);
+      var room = results.insertId;
+    });
+
+
+    dbConnection.query(queryStringUsers, {name: 'Valjean'}, function (error, results, fields) {
+      if (error) throw error;
+      console.log(results.insertId);
+      var user = results.insertId;
+    });
+
+    //execute db query to select test date from tables to verify test
+    dbConnection.query(queryStringMessages, {message: 'Men like you can never change!', user: user, room: room}, function(err) {
       if (err) { throw err; }
 
       // Now query the Node chat server and see if it returns
       // the message we just inserted:
       request('http://127.0.0.1:3000/classes/messages', function(error, response, body) {
         var messageLog = JSON.parse(body);
-        expect(messageLog[0].text).to.equal('Men like you can never change!');
+        expect(messageLog[0].message).to.equal('Men like you can never change!');
         expect(messageLog[0].roomname).to.equal('main');
+        expect(messageLog[0].name).to.equal('Valjean');
         done();
       });
     });
