7c2428e47aa521bc287736f0956f70adfd08c456
Amanda Winkles <amandawinkles@gmail.com>
2020-06-18 19:18:04 -0500

    Complete customizing tests

--- server/db/index.js
+++ server/db/index.js
@@ -3,18 +3,18 @@ var mysql = require('mysql');
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
-var con = mysql.createConnection({
+var connection = mysql.createConnection({
   //host: 'localhost',
   user: 'student',
   password: 'student',
   database: 'chat'
 });
 
-con.connect(function(err) {
+connection.connect(function(err) {
   if (err) {
     throw err;
   }
   console.log('connected!');
 });
-module.exports = con;
+module.exports = connection;
 
